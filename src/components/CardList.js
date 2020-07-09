3618579dfa7639007901f3eefad6cbc007b5988b
charlieisold <marklangvt@gmail.com>
2020-07-09 14:27:58 -0400

    Add files via upload

---
+++ src/components/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = ({ robots }) => {
+    if (true) {
+        throw new Error('NOOOOOOOO!');
+    }
+
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+                    return (
+                    <Card 
+                        id={robots[i].id} 
+                        key={robots[i].id} 
+                        name={robots[i].name} 
+                        email={robots[i].email}/>
+                    );
+                })
+            }
+        </div>
+    );
+}
+
+export default CardList;
